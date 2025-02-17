a57920e806c5e8957c4454ac7aeadf82f0fdb902
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2025-02-18 01:11:38 +0530

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,36 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <section>
-        <MaxWidthWrapper>
+        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 
+        sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left
+            flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <img src="/snake-1.png" className="w-full" />
 
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">Your Image on a <span className="bg-blue-600 px-2 text-white">Custom</span> Phone Case</h1>
+              <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
+                Preserve your favorite moments with a <span className="font-semibold">one-of-a-kind</span> case.
+                Protect your phone—and your memories—with CaseEvo.
+              </p>
+              <ul className="mt-8 space-y-2 text-left font-medium flex flex-col items-center sm:items-start">
+                <div className="space-y-2">
+                  <li className="flex gap-1.5 items-center text-left">
+                    <Check>
+
+                    </Check>
+                    High-Quality, Durable Material
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
         </MaxWidthWrapper>
       </section>
     </div>
