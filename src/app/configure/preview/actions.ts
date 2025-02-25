a0995c29cc9a038f942e7034d0d03ac9da616d5b
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2025-02-26 04:16:26 +0530

    made the summary page

---
+++ src/app/configure/preview/actions.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+export const createCheckoutSession = async ({
+  configId,
+}: {
+  configId: string;
+}) => {
+  const configuration = await db.configuration.findUnique({
+    where: { id: configId },
+  });
+  if (!configuration) {
+    throw new Error("Configuration not found");
+  }
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  if (!user) {
+    throw new Error("You need to be logged in");
+  }
+  const { finish, material } = configuration;
+};
